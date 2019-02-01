e3cd43b0ed779e7d579d3531981f4c142064aafa
pamccombs <34119196+pamccombs@users.noreply.github.com>
2019-02-01 03:39:48 -0500

    Setup for actual scrape

--- lib/macys_deal/cli.rb
+++ lib/macys_deal/cli.rb
@@ -11,32 +11,39 @@ class MacysDeal::CLI
 
     # here doc - http://blog.jayfields.com/2006/12/ruby-multiline-strings-here-doc-or.html
     puts "Macy's Deals:"
-    puts <<-DOC.gsub /^\s*/, ''
 
-    1. Super Weekend Sale - Extra 25%, 15% or 10% off! - Promo code: YAY
 
-    2. Hosiery - 3 for $30 Hanes Perfect Nudes Sheers - No promo code
-
-    3. Sports Fan Shop by LIDS - 40% off Markdowns - No promo code
-
-    DOC
+    @deals = MacysDeal::Deal.today
+    @deals.each.with_index(1) do |deal, i|
+      puts "#{i}. #{deal.name} - #{deal.price} - #{deal.promo}"
+    end
 
   end
 
   def menu
-    puts "Enter the number(#) for the deal you are interested in or type exit to enter:"
+
     input = nil
     while input != "exit"
+      puts "Enter the number(#) for the deal you are interested in or type list to see deals or type exit:"
       input = gets.strip.downcase
-      case input
-      when "1"
-        puts "More info on deal 1..."
-      when "2"
-        puts "More info on deal 2..."
-      when "3"
-        puts "More info on deal 3..."
+
+      if input.to_i > 0
+        the_deal = @deals[input.to_i-1]
+        puts "#{the_deal.name} - #{the_deal.price} - #{the_deal.promo}"
+      elsif input == "list"
+        list_deals
+      else
+        puts "Incorrect input."
+        list_deals
       end
     end
+  end
+
+  def goodbye
+    puts "Thank you for your support! Check daily for more deals!"
+  end
+end
+
   end
 
   def goodbye
