dd353ef2e9949a9d175e0b4fb4914d0f251383c3
Patrick McCombs <pa.mccombs1@gmail.com>
2019-02-08 20:19:43 +0000

    cleaning up

--- lib/macys_deal/cli.rb
+++ lib/macys_deal/cli.rb
@@ -8,11 +8,7 @@ class MacysDeal::CLI
   end
 
   def list_deals
-
-    # here doc - http://blog.jayfields.com/2006/12/ruby-multiline-strings-here-doc-or.html
     puts "Macy's Deals:"
-
-
     @deals = MacysDeal::Deal.today
     @deals2 = []
     @deals.map.with_index() do |deal, i|
@@ -37,13 +33,11 @@ class MacysDeal::CLI
         the_deal = @deals2[input.to_i-1]
         puts "#{the_deal.name[input.to_i-1]} - #{the_deal.price[input.to_i-1]} - #{the_deal.promo_t[input.to_i-1]}"
         puts "You can find this deal here! - #{the_deal.url[0][input.to_i-1]}"
-        #binding.pry
       elsif input == "list"
         list_deals
       else
         puts "Incorrect input."
         list_deals
-        #binding.pry
       end
     end
   end
