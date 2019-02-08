d98452684c55d6430d9f17506e025c6f74e97a18
Patrick McCombs <pa.mccombs1@gmail.com>
2019-02-08 22:17:50 +0000

    created scraper.rb from deal.rb

--- lib/macys_deal/scraper.rb
+++ lib/macys_deal/scraper.rb
@@ -1,3 +1,25 @@
 class MacysDeal::Scraper
 
+  attr_accessor :name, :price, :promo_t, :url
+
+  def self.scrape_macys
+    doc = Nokogiri::HTML(open("https://www.macys.com/shop/coupons-deals?cm_sp=navigation-_-top_nav-_-deals&lid=glbtopnav_deals-us")) do |config|
+      config.strict.noblanks
+    end
+
+    deal = self.new
+    deal.name = doc.search('h2.promo-header-text').map(&:text)
+    deal.price = doc.search('h2.promo-sub-header-text').map(&:text)
+    deal.promo_t = doc.search('div.promo-code').map(&:text)
+    @good_url = []
+    deal.url = doc.search('div.promo-footer').search('a').map do |attrs|
+      if attrs.attr("href") == nil || attrs.attr("href") == "#"
+        next
+        else
+        @good_url << attrs.attr("href")
+      end
+      @good_url
+    end
+    deal
+  end
 end
