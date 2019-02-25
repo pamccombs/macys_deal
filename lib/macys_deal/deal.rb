ecf7dfb8b5ac48af7cd1978e488728df58a5474a
Patrick McCombs <pa.mccombs1@gmail.com>
2019-02-25 21:16:53 +0000

    update deal.rb and scraper rb

--- lib/macys_deal/deal.rb
+++ lib/macys_deal/deal.rb
@@ -3,24 +3,24 @@ class MacysDeal::Deal
   attr_accessor :name, :price, :promo_t, :url
   @@all = []
   def self.scrape_deals
-    @@all << MacysDeal::Scraper.scrape_macys
-    @@all
+    @deal_filter << MacysDeal::Scraper.scrape_macys
+    @deal_filter
     #binding.pry
   end
   
-  def name
-    @name = []
+  def self.organize
+    #I need name[x], price[x], promo_t[x] to be stored as deal[x]
+    
   end
   
-  def price
-    @price = []
-  end
-  
-  def promo_t
-    @promo_t = []
-  end
-  
-  def url
-    @url = []
-  end
+  #def organize_url
+  #  deal.url = doc.search('div.promo-footer').search('a').map do |attrs|
+  #    if attrs.attr("href") == nil || attrs.attr("href") == "#"
+  #      next
+  #      else
+  #      @url << attrs.attr("href")
+  #    end
+  #    @url
+  #  end
+  #end
 end
--- lib/macys_deal/scraper.rb
+++ lib/macys_deal/scraper.rb
@@ -6,19 +6,13 @@ class MacysDeal::Scraper
     end
 
     deal = MacysDeal::Deal.new
-    #I need name[x], price[x], promo_t[x] to be stored as deal[x]
+  
     deal.name = doc.search('h2.promo-header-text').map(&:text)
     deal.price = doc.search('h2.promo-sub-header-text').map(&:text)
     deal.promo_t = doc.search('div.promo-code').map(&:text)
-    binding.pry
-    deal.url = doc.search('div.promo-footer').search('a').map do |attrs|
-      if attrs.attr("href") == nil || attrs.attr("href") == "#"
-        next
-        else
-        self.url << attrs.attr("href")
-      end
-      self.url
-    end
+    deal.url = url = doc.search('div.promo-footer').search('a').map {|attrs| attrs.attr("href")} 
+    
     deal
+    binding.pry
   end
 end
